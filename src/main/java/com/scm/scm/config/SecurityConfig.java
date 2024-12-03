611c51e319d620af65860670ad473767cd9ed655
Bhargav Bhutwala <bhargav1999.bb@gmail.com>
2024-12-03 18:57:32 -0500

    Customized Login page

--- src/main/java/com/scm/scm/config/SecurityConfig.java
+++ src/main/java/com/scm/scm/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -53,14 +52,24 @@ public class SecurityConfig {
    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
 
       // configure public and private urls
-      httpSecurity.authorizeHttpRequests(
-            authorize -> {
-               // authorize.requestMatchers("/about", "/register", "/login",
-               // "/home").permitAll().anyRequest()
-               // .authenticated();
-               authorize.requestMatchers("/user/**").authenticated();
-               authorize.anyRequest().permitAll();
-            }).formLogin(Customizer.withDefaults());
+      httpSecurity
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(
+                  authorize -> {
+                     // authorize.requestMatchers("/about", "/register", "/login",
+                     // "/home").permitAll().anyRequest()
+                     // .authenticated();
+                     authorize.requestMatchers("/user/**").authenticated();
+                     authorize.anyRequest().permitAll();
+                  })
+            .formLogin(formLogin -> {
+               formLogin.loginPage("/login");
+               formLogin.loginProcessingUrl("/authenticate");
+               formLogin.defaultSuccessUrl("/user/dashboard");
+               formLogin.failureForwardUrl("/login?error=true");
+               formLogin.usernameParameter("email");
+               formLogin.passwordParameter("password");
+            });
 
       return httpSecurity.build();
    }
